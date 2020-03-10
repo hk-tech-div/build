9887d1b5b6a52c182621c384c4e62381dd127310
hk-tech-div <59913728+hk-tech-div@users.noreply.github.com>
2020-03-10 22:27:08 +0530

    Add files via upload

---
+++ precache-manifest.85c32a74c8c43a62124181fac152d5be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68a101c1b882dc51d41d4bd4ceff165c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76e3bbdf33f9eb4a55b1",
+    "url": "/static/css/main.cf972f90.chunk.css"
+  },
+  {
+    "revision": "27485ced005762dd083c",
+    "url": "/static/js/2.660d7345.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.660d7345.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76e3bbdf33f9eb4a55b1",
+    "url": "/static/js/main.3b17f14a.chunk.js"
+  },
+  {
+    "revision": "1a08354a56d257e6d3f4",
+    "url": "/static/js/runtime-main.0aefe934.js"
+  },
+  {
+    "revision": "df5c7a4a9b3edc4ae79a50d34b936abc",
+    "url": "/static/media/avatar_g2.df5c7a4a.jpg"
+  }
+]);
